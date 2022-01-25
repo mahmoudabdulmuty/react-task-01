6cd65bb1a638a6022ad4a434a000281ae05a7124
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-25 10:03:29 +0200

    change app comp to statefull comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Component } from 'react';
 import About from './components/About';
+import ProductLists from './components/ProductLists';
 import Services from './components/Services';
 
-function App() {
-	return (
-		<div className="App">
-			<About />
-			<Services />
-		</div>
-	);
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<About />
+				<Services />
+				<ProductLists />
+			</div>
+		);
+	}
 }
 
 export default App;
